730334540c49a7cd11f3b198a4e3097c61f06f7a
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2022-04-18 15:57:13 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (92.14%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -4,7 +4,6 @@ class Solution {
         int n = needle.length();
         if (h == 0 || n == 0) return 0;
         for (int i = 0; i < h-n + 1; i++){
-            System.out.println(haystack.substring(i,i + (n)));
             if (haystack.substring(i,i + (n)).equals(needle)) return i;
         }
         return -1;
