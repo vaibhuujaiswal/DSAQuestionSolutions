d80cbc069ec8cc1f48c6cddd7bbd0bb34d0654c6
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2022-06-16 12:54:37 +0530

    Time: 0 ms (100.00%), Space: 60.6 MB (39.80%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,52 +1,61 @@
-// class Solution {
-//     public int singleNonDuplicate(int[] nums) {
-//         int n = nums.length;
-//         int l = 0;
-//         int r = n - 1;
-//         while (l <= r){
-//             int m = (l + r)/2;
-//             if (nums[m] != nums[m + 1] || nums[m] != nums[m - 1]){
-//                 return nums[m];
-//             }else if (nums[m] == nums[m - 1]) {
-//                 if ((m-l)%2 == 1){
-//                     if (l == m-2){
-//                         return nums[l];
-//                     }
-//                     r = m-2;
-//                 }else{
-//                     l = m+1;
-//                 }
-//             }else if (nums[m] == nums[m + 1]){
-//                 if ((r-m)%2 == 1){
-//                     if (r == m+2){
-//                         return nums[r];
-//                     }
-//                     l = m + 2;      
-//                 }
-//                 else{
-//                     r = m-1;
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = 0;
-        int l = 0, r = nums.length -1;
-        while (l < r){
-            int m = l + (r - l) / 2;
-            if (m % 2 == 1) {
-               m--;  
-            }
-            if (nums[m] == nums[m + 1]) {
-                l = m + 2;
+    //     int n = nums.length;
+    //     int l = 0;
+    //     int r = n - 1;
+    //     while (l <= r){
+    //         int m = (l + r)/2;
+    //         if (nums[m] != nums[m + 1] || nums[m] != nums[m - 1]){
+    //             return nums[m];
+    //         }else if (nums[m] == nums[m - 1]) {
+    //             if ((m-l)%2 == 1){
+    //                 if (l == m-2){
+    //                     return nums[l];
+    //                 }
+    //                 r = m-2;
+    //             }else{
+    //                 l = m+1;
+    //             }
+    //         }else if (nums[m] == nums[m + 1]){
+    //             if ((r-m)%2 == 1){
+    //                 if (r == m+2){
+    //                     return nums[r];
+    //                 }
+    //                 l = m + 2;      
+    //             }
+    //             else{
+    //                 r = m-1;
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
+        
+        int low = 0;
+        int high = nums.length - 2;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid % 2 == 0) {
+                // Checking whether we are in right half
+                if (nums[mid] != nums[mid + 1]) 
+                    
+                    //Shrinking the right half
+                    high = mid - 1; 
+                else
+                    // Shrinking the left half
+                    low = mid + 1; 
             } else {
-                r = m;
+                // Checking whether we are in right half
+                if (nums[mid] == nums[mid + 1])
+                    //Shrinking the right half
+                    high = mid - 1; 
+                else
+                    // Shrinking the left half
+                    low = mid + 1; 
             }
         }
-        return nums[l];
-    }
+        return nums[low];
 }
+}
+
