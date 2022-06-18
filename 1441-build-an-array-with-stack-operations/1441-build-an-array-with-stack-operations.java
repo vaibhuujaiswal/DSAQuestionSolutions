8e041a5a51d42db4b8ee4f002ea43de0bc94c771
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2022-06-18 13:04:40 +0530

    Time: 1 ms (45.86%), Space: 43.1 MB (62.81%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,25 +1,19 @@
+
+
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        // List<String> stack = new ArrayList<String>();
-        // int x = 1;
-        // for (int i = 0; i < target.length; i++){
-        //     stack.add("Push");
-        //     if (target[i] != x){
-        //         stack.add("Pop");
-        //         stack.add("Push");
-        //     }
-        //     x++;
-        // }
-        // return stack;
-            List<String> list = new ArrayList<>();
-        int j = 0;
-    for(int i = 1; i <= target[target.length-1]; i++){
-        list.add("Push");        
-        if(target[j]!=i)
-            list.add("Pop");
-        else
-            j++;
-    } 
-    return list;
+        List<String> stack = new ArrayList<String>(); 
+        //consider index to start from 1
+        int len = target.length;
+        int x = 0; 
+        for (int i = 1; i <= target[target.length - 1]; i++){
+            stack.add("Push");
+            if (target[x] == i){
+                x++;
+            }else{
+                stack.add("Pop");
+                }
+            }
+        return stack;
+        }
 }
-    }
