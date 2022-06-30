34010b1c592aa1db14f91430f9f24de9a013eb9b
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2022-06-30 20:55:29 +0530

    Time: 643 ms (5.00%), Space: 66.3 MB (6.76%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,18 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> set=new HashSet<>();  
+        ListNode a = headA; //intialize each node
+        ListNode b = headB;
         ListNode finalValue = null;
-        ListNode A = headA;
-        while (A != null){
-            set.add(A);
-            A = A.next;
-        }
-        while (headB != null) {
-            if (set.contains(headB)){
-                finalValue = headB;
+        while (a!= null || b!=null){ 
+            //The while loop will break when both the pointer together are equal to null
+            if (a == null) a = headB;
+            else if (b == null) b = headA;
+            if (a == b){
+                finalValue = a;
                 break;
             }
-            headB = headB.next;
+            System.out.print(a.val + " ");
+            System.out.print(b.val);
+            System.out.println(" ");
+            a = a.next;
+            b = b.next;
         }
         return finalValue;
     }
