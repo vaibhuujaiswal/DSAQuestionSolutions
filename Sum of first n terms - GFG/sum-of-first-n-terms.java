22fb1a95abf32d870e6cb283d0776c2a0ad8dfd7
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2022-11-09 07:24:00 +0530

    Added solution - LeetHub

---
+++ Sum of first n terms - GFG/sum-of-first-n-terms.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfSeries(N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long sumOfSeries(long N) {
+        long sum = N*N*(N+1)*(N+1);
+        sum = sum/4;
+        return sum;
+    }
+}
