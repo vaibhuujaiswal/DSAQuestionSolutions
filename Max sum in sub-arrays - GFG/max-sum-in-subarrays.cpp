af419cda5d9330b12d8ba63ab34c0ff559817b4f
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2023-03-16 18:58:49 +0530

    Added solution - LeetHub

---
+++ Max sum in sub-arrays - GFG/max-sum-in-subarrays.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long pairWithMaxSum(long long arr[], long long N)
+    {
+        long long global = arr[0] + arr[1];
+        if (N == 2) return global;
+        for (int i = 1; i < N-1; i++){
+            long long temp = arr[i] + arr[i+1];
+            global = max(temp,global);
+        }
+        return global;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+	    for(ll i=0;i<n;i++)
+		    cin>>a[i];
+		Solution ob;
+	    cout<<ob.pairWithMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
