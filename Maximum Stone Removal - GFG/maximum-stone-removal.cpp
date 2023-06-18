edcb4ffc0f301b9d2ad1e7a7144929a3325c944e
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2023-06-18 10:01:48 +0530

    Added solution - LeetHub

--- Maximum Stone Removal - GFG/maximum-stone-removal.cpp
+++ Maximum Stone Removal - GFG/maximum-stone-removal.cpp
@@ -11,9 +11,9 @@ class DisjointSet{
     public:
         vector<int> parent, size;
         DisjointSet(int n){
-            parent.resize(n);
-            size.resize(n,1);
-            for (int i = 0; i < n ; i++){
+            parent.resize(n+1);
+            size.resize(n+1,1);
+            for (int i = 0; i < n+1 ; i++){
                 parent[i] = i;
                 size[i] = 1;
             }
@@ -42,40 +42,6 @@ class DisjointSet{
 };
 
 
-class disjointSet {
-    vector<int> rank, parent, size;
-public:
-    disjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
 
 class Solution {
   public:
@@ -86,19 +52,19 @@ class Solution {
             maxRow = max(maxRow, it[0]);
             maxCol = max(maxCol, it[1]);
         }
-        disjointSet ds(maxRow + maxCol + 1); //1 just for precautions
+        DisjointSet ds(maxRow + maxCol + 1); //1 just for precautions
         unordered_map<int,int> umap;
         for (auto it : stones){
             int r = it[0];
             int c = maxRow + 1 + it[1];
             umap[r] = 1;
             umap[c] = 1;
-            ds.unionBySize(r,c);
+            ds.findUnionBySize(r,c);
         }
         
         int cnt = 0;
         for (auto it : umap) { //count only those nodes that are actual coordinates
-            if (ds.findUPar(it.first) == it.first) {
+            if (ds.findParent(it.first) == it.first) {
                 cnt++;
             }
         }
