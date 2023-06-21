d7ccc9032b19daa85c8659c386ecbce58987b302
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2023-06-21 11:24:54 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -6,34 +6,32 @@ using namespace std;
 class Solution {
   public:
     int maximumPoints(vector<vector<int>>& points, int n) {
-        vector < vector < int > > dp(n, vector < int > (4, -1));
-  return f(n - 1, 3, points, dp);
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+        return f(n-1,3,points,n,dp);
     }
     
-    int f(int day, int last, vector<vector<int>> &points,vector<vector<int>> &dp) {
-
-  if (dp[day][last] != -1) return dp[day][last];
-
-  if (day == 0) {
-    int maxi = 0;
-    for (int i = 0; i <= 2; i++) {
-      if (i != last)
-        maxi = max(maxi, points[0][i]);
+    int f(int day, int task,vector<vector<int>>& points, int n, vector<vector<int>> &dp){
+        //base case to be established
+        if (day == 0){
+            int maxbasecase = 0;
+            for (int i = 0 ; i < 3; i++){
+                if (i != task){
+                    maxbasecase = max(maxbasecase,points[day][i]);
+                }
+            }
+            return dp[day][task] = maxbasecase;
+        }
+        if (dp[day][task] != -1) return dp[day][task];
+        
+        int maxi = 0;
+        for (int i = 0; i < 3; i++){
+            if (i != task){
+                int point = points[day][i] + f(day-1,i,points,n,dp);
+                maxi = max(maxi,point);
+            }
+        }
+        return dp[day][task] = maxi;
     }
-    return dp[day][last] = maxi;
-  }
-
-  int maxi = 0;
-  for (int i = 0; i <= 2; i++) {
-    if (i != last) {
-      int activity = points[day][i] + f(day - 1, i, points, dp);
-      maxi = max(maxi, activity);
-    }
-
-  }
-
-  return dp[day][last] = maxi;
-}
 };
 
 //{ Driver Code Starts.
