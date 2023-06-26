1302767e6dbc600f9e02dd91a3c49eff30093dae
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2023-06-26 21:15:11 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -8,25 +8,28 @@ class Solution {
     int editDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1, m-1, s, t, dp);
-    }
-    
-    int f(int i, int j, string s, string t, vector<vector<int>>& dp){
-        //base cases
-        if (i < 0) return j+1;
-        if (j < 0) return i+1;
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        if (s[i] == t[j]){
-            return dp[i][j] = f(i-1,j-1,s,t,dp); //match;
-        }else{
-            int replaceInS = 1 + f(i-1,j-1,s,t,dp);
-            int deletechInS = 1 + f(i-1,j,s,t,dp);
-            int insertInS = 1 + f(i,j-1,s,t,dp);
-            return dp[i][j] = min(min(replaceInS,deletechInS),insertInS);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for (int i = 0; i <= n; i++){
+            dp[i][0] = i;
         }
+        for (int j = 0; j <= m; j++){
+            dp[0][j] = j; //base cases defined
+        }
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if (s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int replaceInS = 1 + dp[i-1][j-1];
+                    int deletechInS = 1 + dp[i-1][j];
+                    int insertInS = 1 + dp[i][j-1];
+                    dp[i][j] = min(min(replaceInS,deletechInS),insertInS);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
 
