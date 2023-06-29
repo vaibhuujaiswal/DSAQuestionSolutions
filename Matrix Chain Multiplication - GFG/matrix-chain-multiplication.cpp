cb6dbfc12b76820d40244580a5fb290550debd9a
Vaibhav Jaiswal <86242780+vaibhuujaiswal@users.noreply.github.com>
2023-06-29 13:36:28 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int matrixMultiplication(int N, int a[])
+    {
+        int dp[N][N];
+        for (int i = 1; i < N; i++) dp[i][i] = 0;
+        
+        for (int i = N-1; i >= 1; i--){
+            for (int j = i+1; j < N; j++){
+                int minv = INT_MAX;
+                for (int k = i; k < j; k++){
+                    int value = (a[i-1] * a[k] * a[j]) + dp[i][k] + dp[k+1][j];
+                    minv = min(minv,value);
+                }
+                dp[i][j] = minv;
+            }
+        }
+        return dp[1][N-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
